8ba6e076b76b6f345063849db456af6abed4ec4d
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-07 11:36:25 -0500

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 // Allow us to use promises
 mongoose.Promise = global.Promise;
 // Connect to the DDBB
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds145649.mlab.com:45649/node-course-ddbb' ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};

dafe7d74ce8b86c4ac83ec578a2706c2192089fb
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-07 11:51:57 -0500

    Changed the MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 // Allow us to use promises
 mongoose.Promise = global.Promise;
 // Connect to the DDBB
-mongoose.connect('mongodb://cerokuo:cerokuo@ds145649.mlab.com:45649/node-course-ddbb' ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};

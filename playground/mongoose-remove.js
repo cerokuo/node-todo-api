a228008ccf10951935ae825bd17aabc5beffb725
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-08 11:48:18 -0500

    Test the DELETE /todos/:id route

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -1,8 +1,9 @@
-const {ObjectID} = require('mongodb');
+const{ObjectID} = require('mongodb');
 
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
+
 
 
 /*//Remove all the documents of a collection
@@ -19,3 +20,4 @@ Todo.findOneAndRemove(_id: '589b322fd3e82d7768874919').then((todo) => {
 Todo.findByIdAndRemove('5899eb82f188181d840d54dc').then((todo) => {
 	console.log('todo', todo);
 });
+

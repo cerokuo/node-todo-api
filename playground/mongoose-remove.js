dbbb34aee57d33ee79362085f087ef36f4fb58ad
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-08 10:46:55 -0500

    Add DELETE /todo/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,21 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+/*//Remove all the documents of a collection
+Todo.remove({}).then((result) => {
+	console.log(result);
+})
+
+//Remove just the one you find
+Todo.findOneAndRemove(_id: '589b322fd3e82d7768874919').then((todo) => {
+	console.log(result);
+})
+*/
+//Find by ID and remove
+Todo.findByIdAndRemove('5899eb82f188181d840d54dc').then((todo) => {
+	console.log('todo', todo);
+});
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 // Allow us to use promises
 mongoose.Promise = global.Promise;
 // Connect to the DDBB
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};

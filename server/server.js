28e6a787fc0a593c8fd94a6c5991941392792f29
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-09 10:25:58 -0500

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const{ObjectID} = require('mongodb');
+
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const{ObjectID} = require('mongodb');
+
 
 var app = express();
 // this port is just for heroku
@@ -70,6 +74,38 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+
+
+app.patch('/todos/:id', (req, res) =>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+ if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed) {
+
+    //getTime gives us the actual day time
+    body.completedAt = new Date().getTime();
+
+  } else {
+    body.clompleted = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=> {
+     if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 

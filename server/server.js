f351d1de36e15bd4b68e533971822dfc6d6c7043
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-13 11:29:07 -0500

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const{ObjectID} = require('mongodb');
-
-
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -12,7 +12,7 @@ var {User} = require('./models/user');
 
 var app = express();
 // this port is just for heroku
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 

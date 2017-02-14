74a9b62e4ec8682c8a9025ae61e0b2266b75da4e
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-14 10:30:38 -0500

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,45 @@
+//SHA256 is because is the number of bits.
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+
+
+var data = {
+	id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log(decoded);
+
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+
+// var data = {
+// 	id: 4
+// };
+
+// var token = {
+// 	data,
+// 	hash : SHA256(JSON.stringify(data) + 'somesecretcode').toString()
+// };
+
+
+//I try to rehash, but i dont know the somesecretCode, so it's not the same hash code.
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecretcode').toString();;
+
+// if(resultHash === token.hash) {
+// 	console.log('Data was not changed');
+// } else {
+// 	console.log('Data was changed, do not trust');
+// }

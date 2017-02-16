b759604f586a81f1dd2de533625d974652fa7582
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-16 11:36:54 -0500

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -129,8 +129,6 @@ app.get('/users/me', authenticate, (req, res) =>{
 });
 
   
-
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email','password']);
   //when I find the credentials, I work with the "user" that I receive
